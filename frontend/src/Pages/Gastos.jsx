32595baacf6bc0429f93226bd38bd80bd7129740
Renato Di Giacomo <renato_di_giacomo@hotmail.com>
2022-08-09 01:14:21 -0300

    Update Gastos.jsx

--- frontend/src/Pages/Gastos.jsx
+++ frontend/src/Pages/Gastos.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
 
 const Gastos = () => {
-  return (
-    <div>Gastos</div>
-  )
-}
+  const [data, setData] = React.useState([]);
 
-export default Gastos
+  React.useEffect(() => {
+    const gastosGet = async (e) => {
+      const resp = await axios.get("http://localhost:8080/gastos");
+      if (resp.data.lenght === 0) {
+        setData([]);
+      }
+      setData(resp.data);
+    };
+    gastosGet();
+  },[]);
+
+  if (!data && data.lenght === 0) return <div>Nada para mostrar</div>;
+  return (
+    <div>
+      {data.map((item) => (
+        <div key={item.id}>
+          {item.title}
+          {item.textproduct}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Gastos;
